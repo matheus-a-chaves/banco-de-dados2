63005a155a6a3f3ebafd3de9ca11d97f39336b21
Matheus Chaves <matheuschaves@ciand.com>
2023-08-29 20:06:54 -0300

    feat: adicionado os SELECTS

--- tarefa01_30-08-2023/SelectTables.sql
+++ tarefa01_30-08-2023/SelectTables.sql
@@ -2,3 +2,25 @@ SELECT * FROM Cliente;
 SELECT * FROM Funcionario;
 SELECT * FROM Locacoes;
 SELECT * FROM Veiculos;
+
+-- QUANDO O CLIENTE FEZ A LOCAÇÃO --
+SELECT
+    Locacoes.Id,
+    Locacoes.DataInicio,
+    Locacoes.DataFim,
+    Cliente.Nome AS NomeCliente
+FROM
+    Locacoes
+    INNER JOIN Cliente ON Locacoes.ClienteCPF = Cliente.CPF;
+
+-- QUANDO E COM QUEM O CLIENTE FEZ A LOCAÇÃO --
+SELECT
+    Locacoes.Id,
+    Locacoes.DataInicio,
+    Locacoes.DataFim,
+    Cliente.Nome AS NomeCliente,
+    Funcionario.Nome AS NomeFuncionario
+FROM
+    Locacoes
+    INNER JOIN Cliente ON Locacoes.ClienteCPF = Cliente.CPF
+    INNER JOIN Funcionario ON Locacoes.FuncionarioCPF = Funcionario.CPF;
